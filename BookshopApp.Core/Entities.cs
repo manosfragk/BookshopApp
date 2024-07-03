bd431a4671cd064615e2d7087774636635d6d7ac
manosfragk <manolisfragiadis@gmail.com>
2024-07-03 21:51:57 +0300

    Disable nullable warning check.

---
+++ BookshopApp.Core/Entities.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookshopApp.Core
+{
+    public class Entities
+    {
+        public class Book
+        {
+            public int Id { get; set; }
+            public string Title { get; set; }
+            public string Author { get; set; }
+            public decimal Price { get; set; }
+        }
+    }
+}
