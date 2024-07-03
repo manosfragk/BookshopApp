eb734a78e3cc07f27bd4172be063da53818aa5bd
manosfragk <manolisfragiadis@gmail.com>
2024-07-04 00:21:30 +0300

    Microservices architecture, Catalog Service. toDo Order, User, Payment service.

---
+++ CatalogService/Controllers/BooksController.cs
@@ -0,0 +1,30 @@
+﻿using CatalogService.Models;
+using CatalogService.Repositories;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CatalogService.Controllers
+{
+    [ApiController]
+    [Route("api/[controller]")]
+    public class BooksController : ControllerBase
+    {
+        private readonly BookRepository _repository;
+
+        public BooksController()
+        {
+            _repository = new BookRepository();
+        }
+
+        [HttpGet]
+        public IEnumerable<Book> Get() => _repository.GetAll();
+
+        [HttpGet("{id}")]
+        public ActionResult<Book> Get(int id)
+        {
+            var book = _repository.GetById(id);
+            if (book == null)
+                return NotFound();
+            return book;
+        }
+    }
+}
---
+++ CatalogService/Repositories/BookRepository.cs
@@ -0,0 +1,17 @@
+﻿using CatalogService.Models;
+
+namespace CatalogService.Repositories
+{
+    public class BookRepository
+    {
+        private readonly List<Book> _books = new List<Book>
+        {
+            new Book { Id = 1, Title = "Book 1", Author = "Author 1", Price = 9.99m },
+            new Book { Id = 2, Title = "Book 2", Author = "Author 2", Price = 12.99m },
+        };
+
+        public IEnumerable<Book> GetAll() => _books;
+
+        public Book GetById(int id) => _books.FirstOrDefault(b => b.Id == id);
+    }
+}
