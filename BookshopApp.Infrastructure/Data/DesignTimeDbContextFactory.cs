b6c9c24c5b767ed1c059327c31af66b498c3d88d
manosfragk <manolisfragiadis@gmail.com>
2024-07-03 23:51:38 +0300

    Setup database. add migrations, create console app as workaround for the specified deps.json does not exist error.

---
+++ BookshopApp.Infrastructure/Data/DesignTimeDbContextFactory.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore.Design;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookshopApp.Infrastructure.Data
+{
+    public class DesignTimeDbContextFactory : IDesignTimeDbContextFactory<AppDbContext>
+    {
+        public AppDbContext CreateDbContext(string[] args)
+        {
+            var optionsBuilder = new DbContextOptionsBuilder<AppDbContext>();
+            // Replace with your connection string
+            optionsBuilder.UseSqlite("Data Source=bookshop.db");
+
+            return new AppDbContext(optionsBuilder.Options);
+        }
+    }
+}
